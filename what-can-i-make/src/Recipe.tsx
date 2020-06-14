5574d9fcb76aea60edfb3dfe09973c07171bb2e4
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-06-14 13:10:23 -0400

    Implements TypeScript instead of JS

---
+++ what-can-i-make/src/Recipe.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Col, Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import './RecipeStyles.css';
+
+interface IngredientInfo {
+  original: string;
+}
+
+interface Info {
+  usedIngredients: Array<IngredientInfo>;
+  title: string;
+  image: string;
+}
+
+const Recipe: React.FunctionComponent<{
+  isLoading: boolean;
+  recipe: Info;
+}> = (props) => {
+  console.log(props);
+  return (
+    <Col className='col-lg-6'>
+      {props.isLoading ? (
+        <h1>Loading recipes...</h1>
+      ) : (
+        <Card>
+          <CardImg
+            top
+            width='100%'
+            className='img'
+            src={props.recipe.image}
+            alt='recipe'
+          />
+          <CardBody>
+            <CardTitle className='text'>{props.recipe.title}</CardTitle>
+            {props.recipe.usedIngredients.length === 0 ? (
+              <CardText>No ingredients here!</CardText>
+            ) : (
+              <CardText>
+                Here is what you need:
+                <ul>
+                  {props.recipe.usedIngredients.map((ingredient) => (
+                    <li>{ingredient.original}</li>
+                  ))}
+                </ul>
+              </CardText>
+            )}
+          </CardBody>
+        </Card>
+      )}
+      <br />
+    </Col>
+  );
+};
+export default Recipe;
--- what-can-i-make/src/RecipeCollection.test.js
+++ what-can-i-make/src/RecipeCollection.test.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import RecipeCollection from './RecipeCollection';
 import RecipeForm from './RecipeForm';
 import Recipe from './Recipe';
 
 test('Renders the recipe form', () => {
-	const { getByText } = render(<RecipeForm />);
-	const linkElement = getByText('List Your Ingredients!');
-	expect(linkElement).toBeInTheDocument();
+  const { getByText } = render(<RecipeForm />);
+  const linkElement = getByText('List Your Ingredients!');
+  expect(linkElement).toBeInTheDocument();
 });
 
 test('Renders recipes once user inputs ingredients', () => {
-	render(<RecipeCollection />);
-	const submit = screen.getByText('Submit');
-	fireEvent.click(submit);
-	expect(<Recipe />).toBeInDocument();
+  render(<RecipeCollection />);
+  const submit = screen.getByText('Submit');
+  fireEvent.click(submit);
+  expect(<Recipe />).toBeInDocument();
 });
 
 test('Changes format from user input to query strings', () => {
-	render(<RecipeForm />);
-	const ingredients = 'eggs, milk, bread';
-	expect(<RecipeForm ingredients={ingredients} />).toBe('eggs,+milk,+bread');
+  render(<RecipeForm />);
+  const ingredients = 'eggs, milk, bread';
+  expect(<RecipeForm ingredients={ingredients} />).toBe('eggs,+milk,+bread');
 });

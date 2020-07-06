4e7a6ddd614137e3fc5375adf4c31ab83e8e93cf
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-07-06 12:14:53 -0400

    Sets up build for deploying

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'what-can-i-make', 'build')));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
 	res.json({ key: process.env.API_KEY });
+});
+
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'what-can-i-make', 'build', 'index.html'));
 });
 
 // console.log that your server is up and running

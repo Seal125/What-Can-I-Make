ab87b0910ab22ddedc2fe8cf590bc3bcb0bbc50c
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-07-06 12:54:45 -0400

    Adds build folder to root

---
+++ build/precache-manifest.588c580226617f8baaca764fa7bb48df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee774d3b228d1ae5e9328226d3077dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0953be1c760fb7779bf",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b46de2adc8b26e4226e7",
+    "url": "/static/css/main.206e0bee.chunk.css"
+  },
+  {
+    "revision": "a0953be1c760fb7779bf",
+    "url": "/static/js/2.60213667.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.60213667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b46de2adc8b26e4226e7",
+    "url": "/static/js/main.130e47f8.chunk.js"
+  },
+  {
+    "revision": "af4af329b81826f83504",
+    "url": "/static/js/runtime-main.cd6df276.js"
+  }
+]);
